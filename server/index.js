ed8b427ee3a339b20bad349911039d1aeb5af341
wjswlgh96 <wjswlgh96@naver.com>
2021-07-21 17:36:11 +0900

    오류 파일 수정

--- server/index.js
+++ server/index.js
@@ -10,12 +10,4 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-db.query('use test', (err) => {
-  if (err) {
-    return res.status(400).send("DB connect Error!!");
-  }
-
-  return res.status(200).send("DB connect Success!!");
-});
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
