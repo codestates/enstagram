8bd2333487090857cc27870c1a495fee8026ee8a
wjswlgh96 <wjswlgh96@naver.com>
2021-07-21 21:04:57 +0900

    DB 연결 테스트

--- server/app.js
+++ server/app.js
@@ -8,17 +8,11 @@ const port = 80;
 app.get('/', (req, res) => {
 
   db.query('use test', (err, result) => {
-    result.query('create table name(id INT, name STRING)', (err2, result2) => {
-      if (err2) {
-        return res.status(201).send("DB Connect Fail");
-      }
 
-      console.log("result2 에는 뭐가 들어갔죠??", result2);
-    });
+    return res.status(201).send("DB Connect Success!");
   });
 
-  return res.status(201).send("DB Connect Success!");
-
+  return res.status(201).send("DB Connect Fail!");
 });
 
 app.listen(port, () => {
