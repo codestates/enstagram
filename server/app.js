a9ea358d294d599bc884a67a435da2c2b66d89da
wjswlgh96 <wjswlgh96@naver.com>
2021-07-21 18:35:05 +0900

    서버 구축

--- server/app.js
+++ server/app.js
@@ -1,12 +1,36 @@
+require("dotenv").config();
+const fs = require("fs");
+const https = require("https");
+const cors = require("cors");
 const express = require("express");
 const app = express();
 
-const PORT = 4000;
+const HTTPS_PORT = 4000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors({
+  origin: ["*"],
+  credentials: true,
+  methods: ["GET", "POST", "OPTIONS"],
+}));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+let server;
+if (fs.existsSync("./key.pem") && fs.existsSync("./cert.pem")) {
+
+  const privateKey = fs.readFileSync(__dirname + "/key.pem", "utf8");
+  const certificate = fs.readFileSync(__dirname + "/cert.pem", "utf8");
+  const credentials = { key: privateKey, cert: certificate };
+
+  server = https.createServer(credentials, app);
+  server.listen(HTTPS_PORT, () => console.log("server runnning"));
+
+} else {
+  server = app.listen(HTTPS_PORT)
+}
+
+module.exports = server;
