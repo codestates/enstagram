675ab2988cb472e72000c2f8a048c6b65c637980
wjswlgh96 <wjswlgh96@naver.com>
2021-07-21 17:13:51 +0900

    DB 연결 테스트를 위한 파일 수정

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const app = express();
+const db = require('./db/connection');
+
 const PORT = 4000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+db.query('use test', (err) => {
+  if (err) {
+    return res.status(400).send("DB connect Error!!");
+  }
+
+  return res.status(200).send("DB connect Success!!");
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
