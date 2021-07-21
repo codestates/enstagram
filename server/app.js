6baa385520a21ae6fd476faa07ca50f40d7b1432
wjswlgh96 <wjswlgh96@naver.com>
2021-07-21 19:40:23 +0900

    좀 되자

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-const port = 4000;
+const port = 80;
 
 app.get('/', (req, res) => {
   res.status(201).send('Hello World');
