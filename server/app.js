9673faf97eeb66a28c7c05a5a9395a6aa10768f1
wjswlgh96 <wjswlgh96@naver.com>
2021-07-21 19:07:21 +0900

    서버 계속 수정

--- server/app.js
+++ server/app.js
@@ -1,30 +1,21 @@
-require("dotenv").config();
-const fs = require("fs");
-const https = require("https");
-const express = require("express");
+const express = require('express');
+const cors = require('cors');
 const app = express();
 
-const HTTPS_PORT = 4000;
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+const port = 4000;
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true
+  })
+);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.status(201).send('Hello World');
 });
 
-let server;
-if (fs.existsSync("./key.pem") && fs.existsSync("./cert.pem")) {
-
-  const privateKey = fs.readFileSync(__dirname + "/key.pem", "utf8");
-  const certificate = fs.readFileSync(__dirname + "/cert.pem", "utf8");
-  const credentials = { key: privateKey, cert: certificate };
-
-  server = https.createServer(credentials, app);
-  server.listen(HTTPS_PORT, () => console.log("server runnning"));
-
-} else {
-  server = app.listen(HTTPS_PORT)
-}
-
-module.exports = server;
+app.listen(port, () => {
+  console.log(`서버가 ${port}번에서 작동중입니다.`);
+});
