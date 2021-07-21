edf05a2143d703b925976f19f75d4aa5d563ceb5
wjswlgh96 <wjswlgh96@naver.com>
2021-07-21 20:09:35 +0900

    DB 연결 및 서버 배포 완료

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -5,5 +5,8 @@ npm install pm2@latest -g
 sudo apt-get update
 sudo apt-get install authbind
 sudo touch /etc/authbind/byport/80
+sudo touch /etc/authbind/byport/443
 sudo chown ubuntu /etc/authbind/byport/80
+sudo touch /etc/authbind/byport/443
 sudo chmod 755 /etc/authbind/byport/80
+sudo touch /etc/authbind/byport/443
--- server/app.js
+++ server/app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
+const db = require('./db/connection');
 
 app.use(express.json());
 const port = 80;
 
 app.get('/', (req, res) => {
-  res.status(201).send('Hello World');
-});
 
-app.listen(port, () => {
-  console.log(`서버가 ${port}번에서 작동중입니다.`);
-});
+  db.query('use test', (err) => {
+    if (err) {
+      return res.status(200).send("DB Connect Fail!");
+    }
+
+    return res.status(201).send('DB Connect Success!');
+  });
+
+  app.listen(port, () => {
+    console.log(`서버가 ${port}번에서 작동중입니다.`);
+  });
