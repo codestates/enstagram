088c409928b746e06d7054d0c099e654abdf60cb
wjswlgh96 <wjswlgh96@naver.com>
2021-07-22 16:44:51 +0900

    Test / EC2 POST 요청 테스트_03

--- server/controllers/users/signup.js
+++ server/controllers/users/signup.js
@@ -10,8 +10,6 @@ module.exports = async (req, res) => {
 
     if (!userInfo) {
 
-        console.log("회원가입 성공이요~");
-
         Users.create({
             name: req.body.name,
             username: req.body.username,
@@ -19,12 +17,9 @@ module.exports = async (req, res) => {
             password: req.body.password
         });
 
-        res.send("회원가입 성공");
+        res.status(201).send("회원가입 성공");
 
     } else {
-
-        console.log("응 중복이에용~");
-
-        res.send(200).json("회원가입 실패");
+        res.status(200).send("회원가입 실패");
     }
 };
