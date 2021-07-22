a617ac68cc63407e4d3d725ecbeaa0e5e64c969e
wjswlgh96 <wjswlgh96@naver.com>
2021-07-22 13:50:54 +0900

    test / DB 연결 테스트

--- server/db/connection.js
+++ server/db/connection.js
@@ -7,7 +7,6 @@ const con = mysql.createConnection({
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     port: process.env.DATABASE_PORT,
-    database: process.env.DATABASE_USE,
 });
 
 con.connect((err) => {
