d0d12b852fcf95b0e905219444094ec165a7b791
wjswlgh96 <wjswlgh96@naver.com>
2021-07-23 11:55:31 +0900

    Test: SIGNUP 중복체크 테스트_05

--- server/controllers/users/signup.js
+++ server/controllers/users/signup.js
@@ -4,19 +4,18 @@ const { Users } = require('../../models');
 
 module.exports = async (req, res) => {
 
-    const userInfo = await Users.findOne({
-        where: { username: req.body.username, email: req.body.email }
+    const userName = await Users.findOne({
+        where: { username: req.body.username }
     });
 
-    if (userInfo) {
+    const userEmail = await Users.findOne({
+        where: { email: req.body.email }
+    });
 
-        console.log("유저 정보=>>>>>>>>>>>>", userInfo.dataValues.email);
-
-        if (userInfo.dataValues.username === req.body.username) {
-            res.status(200).json({ message: "이미 존재하는 username 입니다" });
-        } else if (userInfo.dataValues.email === req.body.email) {
-            res.status(200).json({ message: "이미 존재하는 email 입니다" });
-        }
+    if (userName) {
+        res.status(200).json({ message: "이미 존재하는 username 입니다" });
+    } else if (userEmail) {
+        res.status(200).json({ message: "이미 존재하는 email 입니다" });
     } else {
         const { name, username, email, password } = req.body;
 
