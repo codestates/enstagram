f2c5d00954bd884bfcc67c8c07e51031fbb4373f
wjswlgh96 <wjswlgh96@naver.com>
2021-07-23 17:04:18 +0900

    Edit: 리프레쉬 토큰 오류 수정

--- server/controllers/users/refreshToken.js
+++ server/controllers/users/refreshToken.js
@@ -7,36 +7,35 @@ module.exports = async (req, res) => {
     if (!refreshToken) {
         res.status(200).json({ message: "리프레쉬 토큰이 존재하지 않습니다" });
     } else {
-        try {
-            const token = await verify(refreshToken, process.env.REFRESH_SECRET);
 
-            const { id, username, email, createdAt, updatedAt } = token;
+        const token = await verify(refreshToken, process.env.REFRESH_SECRET);
 
-            const payload = {
-                id,
-                username,
-                email,
-                createdAt,
-                updatedAt,
-            }
+        const { id, username, email, createdAt, updatedAt } = token;
 
-            const accessToken = await sign({
-                id,
-                username,
-                email,
-                createdAt,
-                updatedAt,
-            },
-                process.env.ACCESS_SECRET, {
-                expiresIn: process.env.ACCESS_TIME,
-            });
-
-            console.log("accessToken======>>>", accessToken);
-
-            res.status(200).json({
-                accessToken: accessToken,
-                userInfo: payload,
-            });
+        const payload = {
+            id,
+            username,
+            email,
+            createdAt,
+            updatedAt,
         }
+
+        const accessToken = await sign({
+            id,
+            username,
+            email,
+            createdAt,
+            updatedAt,
+        },
+            process.env.ACCESS_SECRET, {
+            expiresIn: process.env.ACCESS_TIME,
+        });
+
+        console.log("accessToken======>>>", accessToken);
+
+        res.status(200).json({
+            accessToken: accessToken,
+            userInfo: payload,
+        });
     }
 };
