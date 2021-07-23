133de70722e84b00354fdcc433ea8d6045d833cd
wjswlgh96 <wjswlgh96@naver.com>
2021-07-23 13:12:04 +0900

    Test: HTTPS 테스트

--- server/app.js
+++ server/app.js
@@ -3,13 +3,11 @@ const fs = require("fs");
 const https = require("https");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const express = require("express");
-const app = express();
 
 const controllers = require("./controllers");
 
 app.use(express.json());
-let PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 app.use(
   cors({
@@ -20,21 +18,8 @@ app.use(
 
 app.get('/', controllers.test);
 app.post('/signup', controllers.signup);
+app.post('/login', controllers.login);
 
-let server;
-if (fs.existsSync("./key.pem") && fs.existsSync("./cert.pem")) {
-
-  PORT = 443;
-
-  const privateKey = fs.readFileSync(__dirname + "/key.pem", "utf8");
-  const certificate = fs.readFileSync(__dirname + "/cert.pem", "utf8");
-  const credentials = { key: privateKey, cert: certificate };
-
-  server = https.createServer(credentials, app);
-  server.listen(PORT, () => console.log("server runnning"));
-
-} else {
-  server = app.listen(PORT)
-}
-
-module.exports = server;
+app.listen(port, () => {
+  console.log(`서버가 ${port}번에서 작동중입니다.`);
+});
