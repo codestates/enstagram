2c3d40de0f05339cd9234b5b49e2b3ccdae21332
wjswlgh96 <wjswlgh96@naver.com>
2021-07-23 14:09:30 +0900

    Edit: 로그인 요청 수정_06

--- server/controllers/users/login.js
+++ server/controllers/users/login.js
@@ -1,90 +1,97 @@
+require("dotenv").config();
 const { Users } = require('../../models');
 const { sign } = require('jsonwebtoken');
-require("dotenv").config();
 
 module.exports = async (req, res) => {
 
-    const userName = await Users.findOne({
-        where: { username: req.body.username, password: req.body.password }
-    });
-
-    const userEmail = await Users.findOne({
-        where: { email: req.body.email, password: req.body.password }
-    });
-
-    if (userName) {
-        const { dataValues: { id, username, email, createdAt, updatedAt } } = userName;
-        const accessToken = sign({
-            id,
-            username,
-            email,
-            createdAt,
-            updatedAt,
-            iat: Math.floor(Date.now()),
-        },
-            process.env.ACCESS_SECRET, {
-            expiresIn: '30s',
+    if (req.body.username) {
+        const userName = await Users.findOne({
+            where: { username: req.body.username, password: req.body.password },
         });
 
-        const refreshToken = sign({
-            id,
-            username,
-            email,
-            createdAt,
-            updatedAt,
-            iat: Math.floor(Date.now()),
-        },
-            process.env.REFRESH_SECRET, {
-            expiresIn: '7d'
+        if (userName) {
+            const { dataValues: { id, username, email, createdAt, updatedAt } } = userName;
+
+            const accessToken = sign({
+                id,
+                username,
+                email,
+                createdAt,
+                updatedAt,
+                iat: Math.floor(Date.now()),
+            },
+                process.env.ACCESS_SECRET, {
+                expiresIn: '30s',
+            });
+
+            const refreshToken = sign({
+                id,
+                username,
+                email,
+                createdAt,
+                updatedAt,
+                iat: Math.floor(Date.now()),
+            },
+                process.env.REFRESH_SECRET, {
+                expiresIn: '7d'
+            });
+
+            res.cookie('refreshToken', refreshToken, {
+                httpOnly: true,
+                sameSite: 'None',
+                secure: true,
+            });
+
+            res.status(200).json({ accessToken: accessToken, message: "로그인 성공 " });
+
+        } else {
+            res.status(200).json({ message: "로그인 실패" });
+        }
+    } else if (req.body.email) {
+        const userEmail = await Users.findOne({
+            where: { email: req.body.email, password: req.body.password },
         });
 
-        res.cookie('refreshToken', refreshToken, {
-            httpOnly: true,
-            sameSite: 'None',
-            secure: true,
-        });
+        if (userEmail) {
 
-        res.status(200).json({ accessToken: accessToken, message: "로그인 성공 " });
+            const { dataValues: { id, username, email, createdAt, updatedAt } } = userEmail;
 
-    } else if (userEmail) {
+            console.log("유저의 정보==============>>>>>", userEmail.dataValues);
 
-        const { dataValues: { id, username, email, createdAt, updatedAt } } = userEmail;
+            const accessToken = sign({
+                id: id,
+                username,
+                email,
+                createdAt,
+                updatedAt,
+                iat: Math.floor(Date.now()),
+            },
+                process.env.ACCESS_SECRET, {
+                expiresIn: '30s',
+            });
 
-        console.log("유저의 정보==============>>>>>", userEmail.dataValues);
+            const refreshToken = sign({
+                id,
+                username,
+                email,
+                createdAt,
+                updatedAt,
+                iat: Math.floor(Date.now()),
+            },
+                process.env.REFRESH_SECRET, {
+                expiresIn: '7d'
+            });
 
-        const accessToken = sign({
-            id: id,
-            username,
-            email,
-            createdAt,
-            updatedAt,
-            iat: Math.floor(Date.now()),
-        },
-            process.env.ACCESS_SECRET, {
-            expiresIn: '30s',
-        });
+            res.cookie('refreshToken', refreshToken, {
+                httpOnly: true,
+                sameSite: 'None',
+                secure: true,
+            });
 
-        const refreshToken = sign({
-            id,
-            username,
-            email,
-            createdAt,
-            updatedAt,
-            iat: Math.floor(Date.now()),
-        },
-            process.env.REFRESH_SECRET, {
-            expiresIn: '7d'
-        });
+            res.status(200).json({ accessToken: accessToken, message: "로그인 성공 " });
 
-        res.cookie('refreshToken', refreshToken, {
-            httpOnly: true,
-            sameSite: 'None',
-            secure: true,
-        });
-
-        res.status(200).json({ accessToken: accessToken, message: "로그인 성공 " });
-
-    } else {
-        res.status(200).json({ message: "로그인 실패" });
+        } else {
+            res.status(200).json({ message: "로그인 실패" });
+        }
     }
 };
