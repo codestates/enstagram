c005d077cc48c2b4619f010d0812cff08044dec1
wjswlgh96 <wjswlgh96@naver.com>
2021-07-23 13:15:02 +0900

    Test: HTTPS 테스트

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
-const fs = require("fs");
-const https = require("https");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const express = require("express");
+const app = express();
 
 const controllers = require("./controllers");
 
