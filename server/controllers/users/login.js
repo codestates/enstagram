1d0c36256d95ad610c55512a8a5aae97cc7b6c04
wjswlgh96 <wjswlgh96@naver.com>
2021-07-23 17:19:38 +0900

    Edit: 로그인 오류 수정_02

--- server/controllers/users/login.js
+++ server/controllers/users/login.js
@@ -10,96 +10,98 @@ module.exports = async (req, res) => {
             where: { username: req.body.username }
         });
 
-        const userPassword = await Users.findOne({
-            where: { password: req.body.password }
-        });
 
-        if (userName && !userPassword) {
-            res.status(200).json({ message: "비밀번호 오류" });
-        } else if (!userName && userPassword) {
-            res.status(200).json({ message: "아이디 오류" });
-        } else if (!userName && !userPassword) {
-            res.status(200).json({ message: "비밀번호, 아이디 오류" });
+        if (userName) {
+            const userPassword = await Users.findOne({
+                where: { password: req.body.password }
+            });
+
+            if (!userPassword) {
+                res.status(200).json({ message: "비밀번호 오류" });
+            } else {
+                const { dataValues: { id, username, email, createdAt, updatedAt } } = userName;
+
+                const accessToken = await sign({
+                    id,
+                    username,
+                    email,
+                    createdAt,
+                    updatedAt,
+                },
+                    process.env.ACCESS_SECRET, {
+                    expiresIn: process.env.ACCESS_TIME,
+                });
+
+                const refreshToken = await sign({
+                    id,
+                    username,
+                    email,
+                    createdAt,
+                    updatedAt,
+                },
+                    process.env.REFRESH_SECRET, {
+                    expiresIn: '7d'
+                });
+
+                res.cookie('refreshToken', refreshToken);
+                res.status(200).json({ accessToken: accessToken, message: "로그인 성공 " });
+            }
+
         } else {
-            const { dataValues: { id, username, email, createdAt, updatedAt } } = userName;
-
-            const accessToken = await sign({
-                id,
-                username,
-                email,
-                createdAt,
-                updatedAt,
-            },
-                process.env.ACCESS_SECRET, {
-                expiresIn: process.env.ACCESS_TIME,
-            });
-
-            const refreshToken = await sign({
-                id,
-                username,
-                email,
-                createdAt,
-                updatedAt,
-            },
-                process.env.REFRESH_SECRET, {
-                expiresIn: '7d'
-            });
-
-            res.cookie('refreshToken', refreshToken);
-            res.status(200).json({ accessToken: accessToken, message: "로그인 성공 " });
+            res.status(200).json({ message: "아이디 오류" });
         }
     } else if (req.body.email) {
         const userEmail = await Users.findOne({
             where: { email: req.body.email }
         });
 
-        const userPassword = await Users.findOne({
-            where: { password: req.body.password }
-        });
-
-        if (userEmail && !userPassword) {
-            res.status(200).json({ message: "비밀번호 오류" });
-        } else if (!userEmail && userPassword) {
+        if (!userEmail) {
             res.status(200).json({ message: "이메일 오류" });
-        } else if (!userEmail && !userPassword) {
-            res.status(200).json({ message: "비밀번호, 이메일 오류" });
         } else {
-            const { dataValues: { id, username, email, createdAt, updatedAt } } = userEmail;
-
-            const accessToken = await sign({
-                id: id,
-                username,
-                email,
-                createdAt,
-                updatedAt,
-            },
-                process.env.ACCESS_SECRET, {
-                expiresIn: process.env.ACCESS_TIME,
+            const userPassword = await Users.findOne({
+                where: { password: req.body.password }
             });
 
-            const refreshToken = await sign({
-                id,
-                username,
-                email,
-                createdAt,
-                updatedAt,
-            },
-                process.env.REFRESH_SECRET, {
-                expiresIn: '7d'
-            });
+            if (!userPassword) {
+                res.status(200).json({ message: "비밀번호 오류" });
+            } else {
+                const { dataValues: { id, username, email, createdAt, updatedAt } } = userEmail;
 
-            res.cookie('refreshToken', refreshToken);
+                const accessToken = await sign({
+                    id: id,
+                    username,
+                    email,
+                    createdAt,
+                    updatedAt,
+                },
+                    process.env.ACCESS_SECRET, {
+                    expiresIn: process.env.ACCESS_TIME,
+                });
 
-            //TODO: 리프레쉬 토큰 작업 HTTPS 적용 후 바꿔줘야함
-            // , {
-            //     httpOnly: true,
-            //         sameSite: 'None',
-            //             secure: true,
-            // }
+                const refreshToken = await sign({
+                    id,
+                    username,
+                    email,
+                    createdAt,
+                    updatedAt,
+                },
+                    process.env.REFRESH_SECRET, {
+                    expiresIn: '7d'
+                });
 
-            res.status(200).json({ accessToken: accessToken, message: "로그인 성공" });
+                res.cookie('refreshToken', refreshToken);
+
+                //TODO: 리프레쉬 토큰 작업 HTTPS 적용 후 바꿔줘야함
+                // , {
+                //     httpOnly: true,
+                //         sameSite: 'None',
+                //             secure: true,
+                // }
+
+                res.status(200).json({ accessToken: accessToken, message: "로그인 성공" });
+            }
         }
-    } else {
+    } else if ((!req.body.username && !req.body.email) && !req.body.password) {
         res.status(200).json({ message: "회원정보를 입력해주세요" });
     }
 };
