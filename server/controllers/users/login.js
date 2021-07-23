d0d73990eed1df76930ba4a8513a7d7acd44a083
wjswlgh96 <wjswlgh96@naver.com>
2021-07-23 17:34:52 +0900

    Edit: 로그인 오류 수정_04

--- server/controllers/users/login.js
+++ server/controllers/users/login.js
@@ -101,7 +101,9 @@ module.exports = async (req, res) => {
                 res.status(200).json({ accessToken: accessToken, message: "로그인 성공" });
             }
         }
-    } else if ((!req.body.username && !req.body.email) || !req.body.password) {
-        res.status(200).json({ message: "회원정보를 입력해주세요" });
+    } else if ((!req.body.username && req.body.email) || !req.body.password) {
+        res.status(200).json({ message: "회원정보를 입력    해주세요" });
+    } else if ((req.body.username && !req.body.email) || !req.body.password) {
+        res.status(200).json({ message: "회원정보를 입력    해주세요" });
     }
 };
