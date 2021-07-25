6f7702b72b922d35b8b003445e608776551e537d
wjswlgh96 <wjswlgh96@naver.com>
2021-07-25 14:18:32 +0900

    Edit: OAuth 최종 수정

--- server/controllers/facebook/OAuth.js
+++ server/controllers/facebook/OAuth.js
@@ -2,8 +2,6 @@ const { Users } = require('../../models');
 
 module.exports = async (req, res) => {
 
-    console.log("req.params 의 정보 ========>", req.query);
-
     const userInfo = await Users.findOne({
         where: { email: req.query.email }
     });
@@ -12,17 +10,10 @@ module.exports = async (req, res) => {
         res.status(200).json({ message: "일치하는 유저정보가 없습니다" });
     } else {
 
-        const { dataValues: { id, name, username, email, createdAt, updatedAt } } = userInfo;
+        delete userInfo.dataValues.password;
 
         res.status(200).json({
-            data: {
-                id,
-                name,
-                username,
-                email,
-                createdAt,
-                updatedAt
-            },
+            data: userInfo,
             message: "로그인 성공"
         });
     }
