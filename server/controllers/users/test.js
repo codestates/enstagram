0ff8eea102e948a7838c34bb7069ad25513c4c93
wjswlgh96 <wjswlgh96@naver.com>
2021-07-25 13:29:02 +0900

    Test: migrations array type using test_02

--- server/controllers/users/test.js
+++ server/controllers/users/test.js
@@ -1,13 +1,17 @@
-const { Users } = require('../../models');
+const { Users, Test } = require('../../models');
 
 module.exports = async (req, res) => {
 
     const userInfo = await Users.findAll();
+    const testInfo = await Test.findAll({
+        attributes: ['array']
+    });
 
     const userArr = userInfo.map(el => {
         delete el.dataValues.password;
         return el.dataValues;
     });
 
-    res.status(200).json(userArr);
+
+    res.status(200).json(testInfo);
 };
