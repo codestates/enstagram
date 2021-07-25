b07452f40e98271deb4fe4bcd766d0cca48c701f
wjswlgh96 <wjswlgh96@naver.com>
2021-07-25 22:34:39 +0900

    Edit: POst 데이터 불러오기 수정_17

--- server/controllers/posts/getPosts.js
+++ server/controllers/posts/getPosts.js
@@ -10,14 +10,13 @@ module.exports = async (req, res) => {
 
         const followers = userInfo.dataValues.follower_id;
 
-        const followerInfo = followers.map(async (el) => {
+        const followerInfo = await followers.map(async (el) => {
 
             const postInfo = await Posts.findOne({
                 where: { user_id: el }
             });
 
             if (postInfo) {
-                console.log("postInfooooooooooo", postInfo);
                 return postInfo.dataValues;
             } else {
                 res.status(200).json({ message: "상대의 포스트 데이터가 없습니다." })
