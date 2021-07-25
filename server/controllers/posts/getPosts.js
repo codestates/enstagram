4a0632d4b048ca35c217c5be46d341e22648a1e8
wjswlgh96 <wjswlgh96@naver.com>
2021-07-25 20:07:48 +0900

    Edit: 포스트 데이터 불러오기 수정

--- server/controllers/posts/getPosts.js
+++ server/controllers/posts/getPosts.js
@@ -2,9 +2,28 @@ const { Posts } = require('../../models');
 
 module.exports = async (req, res) => {
 
-    const postInfo = await Posts.findOne({
-        where: { id: req.query.id }
-    });
+    if (req.body.id.length !== 0) {
+        const arr = req.query.id.map(el => {
+            return await Posts.findOne({
+                where: { id: el }
+            });
+        });
 
-    res.status(200).json(postInfo);
+        res.status(200).json({
+            data: arr,
+            message: "포스트 데이터 불러오기 성공"
+        });
+
+    } else {
+        const postInfo = await Posts.findOne({
+            where: { id: req.query.id }
+        });
+
+        res.status(200).json({
+            data: postInfo,
+            message: "포스트 데이터 불러오기 성공"
+        });
+    }
+
+    res.status(200).json({ message: "포스터 데이터 불러오기 실패" });
 };
