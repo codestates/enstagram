7932ffc325d31ad76abe45ea4741d133c0d0039a
wjswlgh96 <wjswlgh96@naver.com>
2021-07-25 14:33:24 +0900

    Add: Posts 생성

--- server/controllers/posts/createPosts.js
+++ server/controllers/posts/createPosts.js
@@ -10,6 +10,8 @@ module.exports = async (req, res) => {
 
     Posts.create(post);
 
+    console.log("post의 아이디에양=======================>>>>>>>>", post);
+
     res.status(200).json({
         data: post,
         message: "포스트 생성 성공"
