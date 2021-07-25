6b10eba019084258d764d27a9e7329078a697d1b
wjswlgh96 <wjswlgh96@naver.com>
2021-07-25 18:40:01 +0900

    Add: Like DB 생성

---
+++ server/migrations/20210725093442-create-likes.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Likes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.JSON
+      },
+      post_id: {
+        type: Sequelize.JSON
+      },
+      value: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Likes');
+  }
+};
---
+++ server/models/likes.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Likes extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Likes.init({
+    user_id: DataTypes.JSON,
+    post_id: DataTypes.JSON,
+    value: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'Likes',
+  });
+  return Likes;
+};
