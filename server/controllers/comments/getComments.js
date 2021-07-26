bf42a0f9713688c91c6371560277f4a6918934f0
wjswlgh96 <wjswlgh96@naver.com>
2021-07-26 20:19:27 +0900

    Edit: get comment 수정_11

--- server/controllers/comments/getComments.js
+++ server/controllers/comments/getComments.js
@@ -62,14 +62,14 @@ module.exports = async (req, res) => {
                 }
 
                 if (commentInfos) {
-                    res.status(200).json({
-                        data,
-                        message: "코멘트 데이터 불러오기 성공"
-                    })
+                    return data
                 } else {
                     res.status(200).json({ message: "일치하는 포스트 데이터가 없습니다" });
                 }
             }))
+                .then(result => {
+                    console.log("resulttttttttttttttttt", result);
+                })
         } else {
             res.status(200).json({ message: "존재하지 않는 포스트 아이디 입니다" });
         }
