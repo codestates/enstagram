9496cd9fc7a2bc4ee591390769d52e716f5ee0cf
wjswlgh96 <wjswlgh96@naver.com>
2021-07-26 22:07:13 +0900

    Edit: edituserinfo 조건 수정

--- server/controllers/users/editUserInfo.js
+++ server/controllers/users/editUserInfo.js
@@ -2,46 +2,228 @@ const { Users } = require('../../models');
 
 module.exports = async (req, res) => {
 
-    const userName = await Users.findOne({
-        where: { username: req.body.username }
-    });
+    if (req.body.username && req.body.email && req.body.name) {
 
-    const userEmail = await Users.findOne({
-        where: { email: req.body.email }
-    });
-
-    if (!userName && !userEmail) {
-
-        const userInfo = await Users.findOne({
-            where: { id: req.body.user_id }
+        const userName = await Users.findOne({
+            where: { username: req.body.username }
         });
 
-        const result = {
-            name: req.body.name,
-            username: req.body.username,
-            email: req.body.email
+        const userEmail = await Users.findOne({
+            where: { email: req.body.email }
+        });
+
+        if (!userName && !userEmail) {
+
+            const userInfo = await Users.findOne({
+                where: { id: req.body.user_id }
+            });
+
+            const result = {
+                name: req.body.name,
+                username: req.body.username,
+                email: req.body.email
+            }
+
+            Users.update(result, {
+                where: { id: userInfo.dataValues.id }
+            });
+
+            res.status(200).json({
+                data: result,
+                message: "유저 데이터 변경 성공"
+            });
+
+        } else if (userName) {
+            res.status(200).json({
+                message: "이미 존재하는 username 입니다"
+            });
+        } else if (userEmail) {
+            res.status(200).json({
+                message: "이미 존재하는 email 입니다"
+            });
         }
 
-        Users.update(result, {
-            where: { id: userInfo.dataValues.id }
+    } else if (req.body.name && req.body.username && !req.body.email) {
+
+        const userName = await Users.findOne({
+            where: { username: req.body.username }
         });
 
-        res.status(200).json({
-            data: result,
-            message: "유저 데이터 변경 성공"
+        if (!userName) {
+
+            const userInfo = await Users.findOne({
+                where: { id: req.body.user_id }
+            });
+
+            const result = {
+                name: req.body.name,
+                username: req.body.username,
+            }
+
+            Users.update(result, {
+                where: { id: userInfo.dataValues.id }
+            });
+
+            res.status(200).json({
+                data: result,
+                message: "유저 데이터 변경 성공"
+            });
+
+        } else if (userName) {
+            res.status(200).json({
+                message: "이미 존재하는 username 입니다"
+            });
+        }
+    } else if (req.body.name && !req.body.username && req.body.email) {
+        const userEmail = await Users.findOne({
+            where: { email: req.body.email }
         });
 
-    } else if (userName) {
-        res.status(200).json({
-            message: "이미 존재하는 username 입니다"
+        if (!userEmail) {
+
+            const userInfo = await Users.findOne({
+                where: { id: req.body.user_id }
+            });
+
+            const result = {
+                name: req.body.name,
+                email: req.body.email,
+            }
+
+            Users.update(result, {
+                where: { id: userInfo.dataValues.id }
+            });
+
+            res.status(200).json({
+                data: result,
+                message: "유저 데이터 변경 성공"
+            });
+
+        } else if (userEmail) {
+            res.status(200).json({
+                message: "이미 존재하는 email 입니다"
+            });
+        }
+    } else if (!req.body.name && req.body.username && req.body.email) {
+        const userName = await Users.findOne({
+            where: { username: req.body.username }
         });
-    } else if (userEmail) {
-        res.status(200).json({
-            message: "이미 존재하는 email 입니다"
+
+        const userEmail = await Users.findOne({
+            where: { email: req.body.email }
         });
-    } else {
-        res.status(200).json({
-            message: "일치하는 user 정보를 찾을 수 없습니다"
+
+        if (!userName && !userEmail) {
+
+            const userInfo = await Users.findOne({
+                where: { id: req.body.user_id }
+            });
+
+            const result = {
+                username: req.body.username,
+                email: req.body.email
+            }
+
+            Users.update(result, {
+                where: { id: userInfo.dataValues.id }
+            });
+
+            res.status(200).json({
+                data: result,
+                message: "유저 데이터 변경 성공"
+            });
+
+        } else if (userName) {
+            res.status(200).json({
+                message: "이미 존재하는 username 입니다"
+            });
+        } else if (userEmail) {
+            res.status(200).json({
+                message: "이미 존재하는 email 입니다"
+            });
+        }
+    } else if (req.body.name && !req.body.username && !req.body.email) {
+        const userName = await Users.findOne({
+            where: { name: req.body.name }
         });
+
+        if (!userName) {
+
+            const userInfo = await Users.findOne({
+                where: { id: req.body.user_id }
+            });
+
+            const result = {
+                name: req.body.name
+            }
+
+            Users.update(result, {
+                where: { id: userInfo.dataValues.id }
+            });
+
+            res.status(200).json({
+                data: result,
+                message: "유저 데이터 변경 성공"
+            });
+
+        }
+    } else if (!req.body.name && req.body.username && !req.body.email) {
+        const userName = await Users.findOne({
+            where: { username: req.body.username }
+        });
+
+        if (!userName) {
+
+            const userInfo = await Users.findOne({
+                where: { id: req.body.user_id }
+            });
+
+            const result = {
+                username: req.body.username
+            }
+
+            Users.update(result, {
+                where: { id: userInfo.dataValues.id }
+            });
+
+            res.status(200).json({
+                data: result,
+                message: "유저 데이터 변경 성공"
+            });
+
+        } else {
+            res.status(200).json({
+                message: "이미 존재하는 username 입니다"
+            });
+        }
+    } else if (!req.body.name && !req.body.username && req.body.email) {
+        const userEmail = await Users.findOne({
+            where: { email: req.body.email }
+        });
+
+        if (!userEmail) {
+
+            const userInfo = await Users.findOne({
+                where: { id: req.body.user_id }
+            });
+
+            const result = {
+                email: req.body.email
+            }
+
+            Users.update(result, {
+                where: { id: userInfo.dataValues.id }
+            });
+
+            res.status(200).json({
+                data: result,
+                message: "유저 데이터 변경 성공"
+            });
+
+        } else {
+            res.status(200).json({
+                message: "이미 존재하는 email 입니다"
+            });
+        }
     }
 };
