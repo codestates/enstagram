91779cb41e58a92679ad71033e47b59fb02e7bc6
wjswlgh96 <wjswlgh96@naver.com>
2021-07-26 21:35:17 +0900

    Edit: put user data

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -6,6 +6,7 @@ module.exports = {
     refreshToken: require("./users/refreshToken"),          //* 리프레쉬 토큰 요청
     test: require("./users/test"),                          //* 테스트 데이터 출력
     getUserInfo: require("./users/getUserInfo"),            //* 유저 데이터 가져오기
+    editUserInfo: require("./users/editUserInfo"),          //* 유저 기본 정보 변경
 
     //! Facebook
     OAuth: require("./facebook/OAuth"),                     //* 페이스북 OAuth 유저정보 요청
