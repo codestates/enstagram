8ff854021114a8106ce96f5993ce51e836fd6574
wjswlgh96 <wjswlgh96@naver.com>
2021-07-26 11:16:24 +0900

    Test: Get Post 테스트_11

--- server/controllers/posts/getPosts.js
+++ server/controllers/posts/getPosts.js
@@ -25,7 +25,7 @@ module.exports = async (req, res) => {
             }
         });
 
-        console.log("postArrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr:", postArr[0].dataValues);
+        console.log("postArrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr:", postArr[0]);
 
         Promise.all(
             postArr.map(async postEl => {
