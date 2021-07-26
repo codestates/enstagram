063fcaab0e8abd716fb80b6078b6559c9ca30407
wjswlgh96 <wjswlgh96@naver.com>
2021-07-26 19:42:19 +0900

    Edit: get comment 수정

--- server/controllers/comments/getComments.js
+++ server/controllers/comments/getComments.js
@@ -4,7 +4,7 @@ module.exports = async (req, res) => {
 
     if (req.query.post_id) {
         const userInfo = await Users.findOne({
-            where: { post_id: { in: req.query.post_id } }
+            where: { post_id: { in: [1, 2, 3, 4] } }
         });
 
         if (userInfo) {
