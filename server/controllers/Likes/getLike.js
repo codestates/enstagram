4d55d483fdd7bd01c1c44d99212c768cd68e4551
wjswlgh96 <wjswlgh96@naver.com>
2021-07-26 20:59:26 +0900

    Test: get like_02

--- server/controllers/Likes/getLike.js
+++ server/controllers/Likes/getLike.js
@@ -7,26 +7,24 @@ module.exports = async (req, res) => {
 
     if (postInfos) {
 
-        Promise.all(postInfos.like_id.map(async el => {
+        Promise.all(postInfos.dataValues.like_id.map(async el => {
 
-            console.log("elllllllllllllllllllllllll", el);
+            const likeInfos = await Likes.findOne({
+                where: { id: el.dataValues.like_id }
+            });
 
-            // const likeInfos = await Likes.findOne({
-            //     where: { id: el.dataValues.like_id }
-            // });
-
-            // if (likeInfos) {
-            //     return likeInfos.dataValues;
-            // } else {
-            //     res.status(200).json({ message: "좋아요 ID 정보가 일치하지 않습니다" })
-            // }
+            if (likeInfos) {
+                return likeInfos.dataValues;
+            } else {
+                res.status(200).json({ message: "좋아요 ID 정보가 일치하지 않습니다" })
+            }
         }))
-        // .then(result => {
-        //     res.status(200).json({
-        //         data: result,
-        //         message: "좋아요 데이터 불러오기 성공"
-        //     })
-        // })
+            .then(result => {
+                res.status(200).json({
+                    data: result,
+                    message: "좋아요 데이터 불러오기 성공"
+                })
+            })
 
     } else {
         res.status(200).json({ message: "포스트 ID 정보가 일치하지 않습니다" })
