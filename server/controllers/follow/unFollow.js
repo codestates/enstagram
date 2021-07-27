6675974e8eeaf243a7b2bc82ce8b446aa2163b41
wjswlgh96 <wjswlgh96@naver.com>
2021-07-27 14:09:53 +0900

    Edit: unfollow 수정_01

--- server/controllers/follow/unFollow.js
+++ server/controllers/follow/unFollow.js
@@ -14,20 +14,23 @@ module.exports = async (req, res) => {
 
         if (myInfo.dataValues.follower_id !== 0) {
 
-            let index = Number.MIN_SAFE_INTEGER;
+            const result = [];
 
-            myInfo.dataValues.follower_id.map((el, idx) => {
-                if (el === req.body.target_id) {
-                    index = idx;
-                }
-            })
+            result.push(myInfo.dataValues.follower_id.filter(el => {
+                return el !== req.body.target_id
+            }));
 
-            if (index > -1) {
-                myInfo.dataValues.follower_id.splice(index, 1);
-            }
+            console.log("resultttttttttttttttt", result);
+
+            await Users.update({ follower_id: result }, {
+                where: { id: myInfo.dataValues.id }
+            });
+
+            res.status(200).json({ message: "언팔로우 성공" });
+        } else {
+            res.status(200).json({ message: "현재 팔로우 중인 사람이 없습니다" });
         }
 
-        res.status(200).json({ message: "언팔로우 성공" });
     } else {
         res.status(200).json({ message: "유저 정보를 불러오지 못했습니다" });
     }
