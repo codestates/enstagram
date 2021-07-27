5dfd3f9e6ab24672191bbe131bbf20c1e82410d5
wjswlgh96 <wjswlgh96@naver.com>
2021-07-27 16:27:37 +0900

    Edit: edit password

--- server/controllers/users/editPassword.js
+++ server/controllers/users/editPassword.js
@@ -6,22 +6,26 @@ module.exports = async (req, res) => {
         where: { username: req.body.username }
     });
 
-    const passInfo = await Users.findOne({
+    const passwordInfo = await Users.findOne({
         where: { password: req.body.oldpw }
     });
 
-    if (userInfo) {
+    if (passwordInfo) {
+        if (userInfo) {
 
-        const newPassword = {
-            password: req.body.newpw
-        };
+            const newPassword = {
+                password: req.body.newpw
+            };
 
-        await Users.update(newPassword, {
-            where: { username: req.body.username, password: req.body.oldpw }
-        });
+            await Users.update(newPassword, {
+                where: { username: req.body.username, password: req.body.oldpw }
+            });
 
-        res.status(200).json({ message: "비밀번호 변경 성공" });
+            res.status(200).json({ message: "비밀번호 변경 성공" });
+        } else {
+            res.status(200).json({ message: "일치하는 유저 정보가 없습니다" });
+        }
     } else {
-        res.status(200).json({ message: "일치하는 유저 정보가 없습니다" });
+        res.status(403).json({ message: "기존 비밀번호와 다릅니다" });
     }
 };
