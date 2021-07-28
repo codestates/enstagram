ea92c54ba55d0805321f18c1e6c23a79a94dc721
wjswlgh96 <wjswlgh96@naver.com>
2021-07-28 15:59:03 +0900

    Edit: editprofile, edituserinfo 수정

--- server/controllers/users/editProfilephoto.js
+++ server/controllers/users/editProfilephoto.js
@@ -12,18 +12,20 @@ module.exports = async (req, res) => {
             profilePhoto: req.body.picture
         };
 
-        Users.update(newPhoto, {
+        await Users.update(newPhoto, {
             where: { id: userInfo.dataValues.id }
-        });
+        })
+            .then(res => {
 
-        const afterUserInfo = await Users.findOne({
-            where: { username: req.body.username }
-        });
+                const afterUserInfo = await Users.findOne({
+                    where: { username: res.dataValues.username }
+                });
 
-        res.status(200).json({
-            data: afterUserInfo.dataValues,
-            message: "프로필 사진 변경 성공"
-        });
+                res.status(200).json({
+                    data: afterUserInfo.dataValues,
+                    message: "프로필 사진 변경 성공"
+                });
+            });
 
     } else {
         res.status(200).json({ message: "일치하는 유저 정보가 없습니다" });
