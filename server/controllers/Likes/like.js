355f3061c4ec81806b0b521ab40f0c37e49f8dfa
wjswlgh96 <wjswlgh96@naver.com>
2021-07-28 09:56:07 +0900

    Edit: like 수정

--- server/controllers/Likes/like.js
+++ server/controllers/Likes/like.js
@@ -6,52 +6,83 @@ module.exports = async (req, res) => {
         where: { id: req.body.user_id }
     });
 
-    if (userInfo) {
+    const postInfo = await Posts.findOne({
+        where: { id: req.body.post_id }
+    });
 
-        const like = {
-            user_id: req.body.user_id,
-            post_id: req.body.post_id,
-            value: req.body.value,
-        }
+    const likeInfos = await Likes.findOne({
+        where: { user_id: req.body.user_id, post_id: req.body.post_id }
+    });
 
-        const postInfo = await Posts.findOne({
-            where: { id: req.body.post_id }
-        });
+    if (likeInfos) {
 
-        if (postInfo.dataValues.like_id.indexOf(userInfo.dataValues.id) > -1) {
-            res.status(200).json({ message: "이미 좋아요 처리한 post 입니다" });
-        } else {
-            await Likes.create(like)
-                .then(async (value) => {
+        if (likeInfos.dataValues.value === true) {
 
-                    if (userInfo.dataValues.like_id.length !== 0) {
-                        await Users.update({ like_id: [...userInfo.dataValues.like_id, value.dataValues.id] }, {
-                            where: { id: like.user_id }
-                        });
-                    } else {
-                        await Users.update({ like_id: [value.dataValues.id] }, {
-                            where: { id: like.user_id }
-                        });
-                    }
-
-                    if (postInfo.dataValues.like_id.length !== 0) {
-                        await Posts.update({ like_id: [...postInfo.dataValues.like_id, value.dataValues.id] }, {
-                            where: { id: like.post_id }
-                        });
-                    } else {
-                        await Posts.update({ like_id: [value.dataValues.id] }, {
-                            where: { id: like.post_id }
-                        });
-                    }
-
+            await Likes.update({ value: false }, {
+                where: { id: likeInfos.dataValues.id }
+            })
+                .then(value => {
                     res.status(200).json({
-                        data: like,
+                        data: {
+                            user_id: userInfo.dataValues.id,
+                            post_id: postInfo.dataValues.id,
+                            value: value.dataValues.value
+                        },
                         message: "좋아요 정보 설정 완료"
                     });
                 });
-        }
-    } else {
-        res.status(200).json({ message: "일치하는 유저 정보를 찾지 못했습니다" });
-    }
 
+        } else if (likeInfos.dataValues.value === false) {
+
+            await Likes.update({ value: true }, {
+                where: { id: likeInfos.dataValues.id }
+            })
+                .then(value => {
+                    res.status(200).json({
+                        data: {
+                            user_id: userInfo.dataValues.id,
+                            post_id: postInfo.dataValues.id,
+                            value: value.dataValues.value
+                        },
+                        message: "좋아요 정보 설정 완료"
+                    });
+                });
+
+        }
+
+    } else {
+
+        await Likes.create(like)
+            .then(async (value) => {
+
+                if (userInfo.dataValues.like_id.length !== 0) {
+                    await Users.update({ like_id: [...userInfo.dataValues.like_id, value.dataValues.id] }, {
+                        where: { id: like.user_id }
+                    });
+                } else {
+                    await Users.update({ like_id: [value.dataValues.id] }, {
+                        where: { id: like.user_id }
+                    });
+                }
+
+                if (postInfo.dataValues.like_id.length !== 0) {
+                    await Posts.update({ like_id: [...postInfo.dataValues.like_id, value.dataValues.id] }, {
+                        where: { id: like.post_id }
+                    });
+                } else {
+                    await Posts.update({ like_id: [value.dataValues.id] }, {
+                        where: { id: like.post_id }
+                    });
+                }
+
+                res.status(200).json({
+                    data: {
+                        user_id: userInfo.dataValues.id,
+                        post_id: postInfo.dataValues.id,
+                        value: value.dataValues.value
+                    },
+                    message: "좋아요 정보 설정 완료"
+                });
+            });
+    }
 };
