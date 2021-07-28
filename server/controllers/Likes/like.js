7fdcc24bc5cdf069e02be6d23a98679056e80f0d
wjswlgh96 <wjswlgh96@naver.com>
2021-07-28 10:38:26 +0900

    Edit: like post 요청 수정

--- server/controllers/Likes/like.js
+++ server/controllers/Likes/like.js
@@ -20,45 +20,34 @@ module.exports = async (req, res) => {
 
             await Likes.update({ value: false }, {
                 where: { id: likeInfos.dataValues.id }
-            })
-                .then(value => {
+            });
 
-                    console.log("valueeeeeeeeeeeeeeeee가 true 일 때:", value);
-
-                    res.status(200).json({
-                        data: {
-                            user_id: userInfo.dataValues.id,
-                            post_id: postInfo.dataValues.id,
-                            value: value.dataValues.value
-                        },
-                        message: "좋아요 정보 설정 완료"
-                    });
-                });
+            res.status(200).json({
+                data: {
+                    user_id: userInfo.dataValues.id,
+                    post_id: postInfo.dataValues.id,
+                    value: false
+                },
+                message: "좋아요 정보 설정 완료"
+            });
 
         } else if (likeInfos.dataValues.value === false) {
 
             await Likes.update({ value: true }, {
                 where: { id: likeInfos.dataValues.id }
             })
-                .then(value => {
 
-                    console.log("valueeeeeeeeeeeeeeeee가 false 일 때:", value);
-
-                    res.status(200).json({
-                        data: {
-                            user_id: userInfo.dataValues.id,
-                            post_id: postInfo.dataValues.id,
-                            value: value.dataValues.value
-                        },
-                        message: "좋아요 정보 설정 완료"
-                    });
-                });
-
+            res.status(200).json({
+                data: {
+                    user_id: userInfo.dataValues.id,
+                    post_id: postInfo.dataValues.id,
+                    value: true
+                },
+                message: "좋아요 정보 설정 완료"
+            });
         }
 
     } else if (!likeInfos) {
-
-        console.log("왜 자꾸 여기로 들어와????????????????");
 
         const like = {
             user_id: req.body.user_id,
