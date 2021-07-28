bd80dc7c05e9422a8a3ab30de4f5979c1e39a638
wjswlgh96 <wjswlgh96@naver.com>
2021-07-28 14:41:21 +0900

    Edit: merge

--- server/controllers/maigpage/getMainPage.js
+++ server/controllers/maigpage/getMainPage.js
@@ -24,7 +24,7 @@ module.exports = async (req, res) => {
                 return el.dataValues.id !== tokenUserInfo.id;
             });
 
-            console.log("otherUserssssssssssssssssssssss", otherUsers[0]);
+            console.log("otherUserssssssssssssssssssssss", otherUsers[0].dataValues);
 
             Promise.all(otherUsers.map(async el => {
                 const postInfos = await Posts.findOne({
