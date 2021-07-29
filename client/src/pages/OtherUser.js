ec386340c4d1cdb68b4d20ea2c254beceafb9e09
wjswlgh96 <wjswlgh96@naver.com>
2021-07-29 15:21:05 +0900

    Edit: OAuth facebook

--- client/src/pages/OtherUser.js
+++ client/src/pages/OtherUser.js
@@ -47,26 +47,32 @@ const OtherUserPage = ({ loggedInUserInfo }) => {
     const handleFollow = async (bFollow) => {
         // Update DB
         await axios.post(`${serverUrl}/follow`,
-            { user_id: loggedInUserInfo.id, target_id: userId });
+            { user_id: loggedInUserInfo.id, target_id: userId })
+            .then(result => {
 
-        if (bFollow === true) {
-            console.log("loggedInUserInfo:", loggedInUserInfo);
-            console.log("follower:", follower);
+                console.log("보내주는 user 의 ID", userId);
+                console.log("현재 로그인한 user 의 ID", loggedInUserInfo);
+                console.log("result 의 메세지:", result.data.message);
 
-            if (follower.indexOf(loggedInUserInfo.id) < 0) {
-                if (follower.length === 0) {
-                    const newFollower = [loggedInUserInfo.id];
-                    await setFollower(newFollower);
-                } else {
-                    const newFollower = [...follower, loggedInUserInfo.id]
-                    await setFollower(newFollower);
+                if (result.data.message === '팔로우 성공') {
+                    console.log("loggedInUserInfo:", loggedInUserInfo);
+                    console.log("follower:", follower);
+
+                    if (follower.indexOf(loggedInUserInfo.id) < 0) {
+                        if (follower.length === 0) {
+                            const newFollower = [loggedInUserInfo.id];
+                            setFollower(newFollower);
+                        } else {
+                            const newFollower = [...follower, loggedInUserInfo.id]
+                            setFollower(newFollower);
+                        }
+                    }
+                } else if (result.data.message === '언팔로우 성공') {
+                    console.log("Unfollower:", follower);
+                    const newFollower = follower.filter(el => el !== loggedInUserInfo.id)
+                    setFollower(newFollower);
                 }
-            }
-        } else if (bFollow === false) {
-            console.log("Unfollower:", follower);
-            const newFollower = follower.filter(el => el !== loggedInUserInfo.id)
-            await setFollower(newFollower);
-        }
+            });
     }
 
     return (
